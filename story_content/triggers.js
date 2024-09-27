dd71c0c4c1a6f6d141acad90b3f6d84906883e46
wahyukurniawan23 <wahyukurniawan23@guru.smp.belajar.id>
2024-09-27 17:04:29 +0700

    File SIRANDA

---
+++ story_content/triggers.js
@@ -0,0 +1,54 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6qsv4fUfl1g":
+        Script1();
+        break;
+      case "6aquTYCvkdd":
+        Script2();
+        break;
+      case "63g8Rdvq26V":
+        Script3();
+        break;
+      case "6JeVGG2UDAX":
+        Script4();
+        break;
+      case "6Rk5QXhqbyu":
+        Script5();
+        break;
+      case "6f2u4UFUehw":
+        Script6();
+        break;
+      case "6e8ILwr2JVs":
+        Script7();
+        break;
+      case "5rfKHS0UA86":
+        Script8();
+        break;
+      case "6948sYttL82":
+        Script9();
+        break;
+      case "6ogWNunQifa":
+        Script10();
+        break;
+      case "6ksvooSC5Wz":
+        Script11();
+        break;
+      case "69cMI9eaCcB":
+        Script12();
+        break;
+      case "6eLzH61uZXs":
+        Script13();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,102 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="1.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.075;
+
+}
+
+window.Script2 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script3 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script4 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script5 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script6 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script7 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script8 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script9 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script10 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script11 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script12 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+window.Script13 = function()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.05;
+
+}
+
+};
